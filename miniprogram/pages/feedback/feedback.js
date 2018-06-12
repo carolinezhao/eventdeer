f6ae3e413935512488970b0627bbf88373f0193f
carolinezhao <chaohsimeng@gmail.com>
2018-06-13 00:34:01 +0800

    update feedback info

--- miniprogram/pages/feedback/feedback.js
+++ miniprogram/pages/feedback/feedback.js
@@ -6,21 +6,26 @@ const AV = require('../../libs/av-weapp-min.js');
 const app = getApp();
 
 Page({
+    data: {},
     formSubmit: function (event) {
         console.log(event);
         // 获取表单提交的内容
         let content = event.detail.value.content;
         console.log(content);
+        // 获取微信登录用户名
+        console.log(app.globalData.user);
+        let username = app.globalData.user.nickName;
 
         // 声明类型
         let Feedback = AV.Object.extend('Feedback');
         // 新建对象
-        var feedback = new Feedback();
-        // 设置名称
-        feedback.set('username', 'caroline');
-        feedback.set('content', content);
+        let feedbacks = new Feedback();
+        // 设置属性
+        feedbacks.set('username', username);
+        feedbacks.set('center', 'Haidian');
+        feedbacks.set('content', content);
 
-        feedback.save().then(function (feedback) {
+        feedbacks.save().then(feedback => {
             console.log('objectId is ' + feedback.id);
         }).catch(console.error);
     }
