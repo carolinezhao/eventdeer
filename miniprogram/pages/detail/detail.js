5c492b4984084e4d651ff628afb56c0f6bbe9b10
carolinezhao <chaohsimeng@gmail.com>
2018-06-16 23:56:21 +0800

    save as image

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -10,6 +10,10 @@ Page({
         queryid: '',
         event: [],
         isDisplay: false, // 默认为从程序主入口进入，不显示该元素
+        hasUserInfo: false,
+        // nickName: '',
+        nickName: 'rabbit', // test
+        tempFilePath: '' // 图片临时路径
     },
     onLoad: function (param) {
         // param 是由上一页传递过来的内容
@@ -20,6 +24,19 @@ Page({
         this.setReceivedData(param);
         // 查询并设置详情
         this.queryTargetEvent();
+
+        if (app.globalData.userInfo) {
+            let nickname = app.globalData.userInfo.nickName;
+            this.setData({
+                hasUserInfo: true,
+                nickname: nickname
+            })
+        }
+
+        //
+        wx.showShareMenu({
+            withShareTicket: true
+        })
     },
     checkScene: function () {
         let scene = app.globalData.scene;
@@ -79,5 +96,57 @@ Page({
             path: '',
             imageUrl: ''
         }
+    },
+    saveAsImage: function () {
+        if (!this.data.hasUserInfo) {
+            app.toLogin();
+        } else {
+        // 创建对象
+        let ctx = wx.createCanvasContext('event-detail');
+        console.log('create image');
+        // detail-page 内容，小程序码，分享人
+        // canvas 不能读取页面元素
+        ctx.setFontSize(20);
+        ctx.fillText(`shared by ${this.data.nickName}`, 20, 30);
+        // ctx.setTextAlign('left');
+
+        // ctx.draw(); // test
+        ctx.draw(false, (options) => this.getTempFilePath(options));
+        }
+    },
+    getTempFilePath: function (options) {
+        console.log(options);
+        // 在 draw 回调里调用该方法才能保证图片导出成功。
+        wx.canvasToTempFilePath({
+            canvasId: 'event-detail',
+            fileType: 'jpg', // 'jpg' or 'png'(default)
+            success: (res) => {
+                console.log(res.tempFilePath);
+                this.setData({
+                    tempFilePath: res.tempFilePath // 临时文件路径
+                })
+                this.saveImageToPhotosAlbum();
+            }
+        })
+    },
+    saveImageToPhotosAlbum: function () {
+        // if (!this.data.tempFilePath) {
+
+        // }
+
+        // 区分：wx.saveFile 保存文件到本地
+        // wx.saveImageToPhotosAlbum 保存文件到系统相册
+        // 开发者工具：如果没有授权，会自动请求授权；
+        // 手机：？
+        wx.saveImageToPhotosAlbum({
+            filePath: this.data.tempFilePath,
+            success: (res) => {
+                console.log(res);
+                wx.showToast({
+                    title: '保存成功',
+                    mask: 'true',
+                })
+            }
+        })
     }
 })
