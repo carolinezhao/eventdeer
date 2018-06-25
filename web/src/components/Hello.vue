1128e40577345af8eeb51e28dfd936eb88b4bb71
carolinezhao <chaohsimeng@gmail.com>
2018-06-25 23:47:47 +0800

    init page

---
+++ web/src/components/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <h2>Login</h2>
+    <label>Username</label>
+    <input placeholder="Enter your username">
+    <label>Password</label>
+    <input placeholder="Enter your password">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to EventDeer'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
