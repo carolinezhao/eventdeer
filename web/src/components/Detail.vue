3276cbbf746ed8ef96f7ec26f12ed768fec66de9
carolinezhao <chaohsimeng@gmail.com>
2018-08-03 23:25:29 +0800

    chore: update styles

--- web/src/components/Detail.vue
+++ web/src/components/Detail.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="detail-container card">
     <button @click="goBack">Go Back</button>
-    <div class="detail-img">
+    <div class="img-container" v-if="detail.imgUrl">
       <img class="img" :src="detail.imgUrl">
     </div>
-    <template v-for="(value, key) in detail">
-      <div :key="key.id">{{key}}: {{value}}</div>
-    </template>
+    <div class="title">{{detail.title}}
+      <span class="vip card">{{(detail.isVIP) ? 'VIP' : 'Deluxe'}}</span>
+    </div>
+    <div class="info">
+      <div>{{detail.date}}</div>
+      <div>{{detail.time}}</div>
+    </div>
+    <div class="intro">{{detail.intro}}</div>
   </div>
 </template>
 
 <script>
+import {displayDate, displayTime} from '@/utils/util'
 export default {
   data () {
     return {
@@ -38,9 +44,16 @@ export default {
         .find()
         .then(eventObj => {
           // console.log(eventObj)
-          this.detail = eventObj[0].attributes
+          this.detail = this.backendToDetail(eventObj)
         })
         .catch(console.error())
+    },
+    backendToDetail (obj) {
+      let backObj = obj[0].attributes
+      backObj.date = displayDate(backObj.startTime)
+      backObj.time = `${displayTime(backObj.startTime)} - ${displayTime(backObj.endTime)}`
+      console.log(backObj.intro)
+      return backObj
     }
   }
 }
@@ -49,13 +62,40 @@ export default {
 <style scoped>
   .detail-container {
     margin-top: 20px;
-    padding: 10px;
-    width: 50%;
+    padding: 20px;
+    width: 60%;
   }
 
-  .detail-img {
+  .img-container {
     padding: 10px 0;
     height: 120px;
     width: 200px;
+  }
+
+  .img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .title {
+    font-size: 22px;
+  }
+
+  .vip {
+    font-size: 15px;
+    border-color: #f66;
+    color: #f66;
+    padding: 1px 5px;
+  }
+
+  .info,
+  .intro {
+    margin-top: 10px;
+    line-height: 25px;
+  }
+
+  .intro {
+    /* 保留换行 */
+    white-space: pre-wrap;
   }
 </style>
